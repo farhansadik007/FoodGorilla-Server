33a8fcf9d5e4815cb2cf8c26d87e2c6c417da0c5
farhansadik007 <sadik.zisan@gmail.com>
2023-05-04 16:49:47 +0600

    added comment and gitignore configured

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ const port = process.env.PORT || 5000;
 const chefs = require('./data/chefs.json');
 const recipes = require('./data/recipes.json');
 
+//ensuring that the server is running
 app.get('/', (req, res) => {
     res.send('FoodGorilla is cooking...')
 })
 
+//cors policy
 app.use(cors());
 
+//send chefs list
 app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
+//send recipes list
 app.get('/recipes', (req, res) => {
     res.send(recipes);
 })
 
+//send custom recipes 
 app.get('/recipes/:id', (req, res) => {
     const id = req.params.id;
     const selectedRecipes = recipes.find(n => n.id === id)
