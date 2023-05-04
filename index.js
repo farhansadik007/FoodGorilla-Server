4f0a85e23b1033d6b1bfe7a5a30599c8bd6f178b
farhansadik007 <sadik.zisan@gmail.com>
2023-05-04 14:23:24 +0600

    recipe data send initialized

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 
 app.get('/', (req, res) => {
     res.send('FoodGorilla is cooking...')
@@ -14,6 +15,17 @@ app.use(cors());
 app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
+})
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedRecipes = recipes.find(n => n.id === id)
+    res.send(selectedRecipes);
+})
+
 
 app.listen(port, ()=> {
     console.log(`FoodGorilla API is on port: ${port}`);
